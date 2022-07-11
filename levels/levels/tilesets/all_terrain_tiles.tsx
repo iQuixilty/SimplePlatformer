d3d75b9a93e414cdfb041da1a7045feba7a5fe0e
iQuixilty <76968084+iQuixilty@users.noreply.github.com>
2022-07-11 11:16:23 -0700

    Add files via upload

---
+++ levels/levels/tilesets/all_terrain_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="all_terrain_tiles" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="../../graphics/terrain/all_terrain_tiles.png" width="512" height="512"/>
+</tileset>
---
+++ levels/levels/tilesets/trees.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="trees" tilewidth="112" tileheight="130" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="85" height="130" source="../../graphics/trees/tall_tree.png"/>
+ </tile>
+ <tile id="2">
+  <image width="112" height="105" source="../../graphics/trees/wide_tree.png"/>
+ </tile>
+ <tile id="3">
+  <image width="85" height="130" source="../../graphics/trees/bg_tall_tree.png"/>
+ </tile>
+ <tile id="4">
+  <image width="112" height="105" source="../../graphics/trees/bg_wide_tree.png"/>
+ </tile>
+</tileset>
